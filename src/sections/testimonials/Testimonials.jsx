3bee87fe1096d7a2c25c239a6d5129fe6ca529eb
Muhammad Ali <muhammadalipannizai@gmail.com>
2023-02-27 21:04:50 +0100

    feat :  Add Testimonials

--- src/sections/testimonials/Testimonials.jsx
+++ src/sections/testimonials/Testimonials.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Pagination } from "swiper";
+
+
+import testimonialsData from './data'
+import TestimonialsComponent from "./TestimonialsComponent";
 import './testimonials.css'
+
 function Testimonials() {
   return (
-
+ 
       <section id='testimonials'>
-      Testimonials
+     <h2> Testimonials</h2>
+     <p>
+      These are unbiased testimonials from some of my clients
+     </p>
+     <div className=" container ">
+        <Swiper
+              slidesPerView={1}
+              spaceBetween={30}
+              breakpoints = {{
+                601 : {slidesPerView : 2},
+                1025 :{ slidesPerView : 3}
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              modules={[Autoplay,Pagination]}
+              autoplay={true}
+              className="mySwiper"
+            >
+               {
+                testimonialsData.map(testimonial => (
+                  <SwiperSlide key={testimonial.id}>
+                    <TestimonialsComponent testimonialP ={testimonial} />
+                  </SwiperSlide>
+                    )
+                       )
+               }
+          </Swiper>
+   
+   
+     </div>
     </section>
    
   )
---
+++ src/sections/testimonials/TestimonialsComponent.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Cards from '../../components/Cards'
+function TestimonialsComponent(props) {
+  return (
+   <Cards className = 'light'>
+      <p>{props.testimonialP.quote}</p>
+      <div className='testimonials_clients'>
+      <div className="testimonial_images">
+        <img src={props.testimonialP.avatar} alt = 'testimonials avatar'/>
+      </div>
+      <div className="client_details">
+        <h6>{props.testimonialP.name}</h6>
+        <small>{props.testimonialP.profession}</small>
+      </div>
+      </div>
+   </Cards>
+  )
+}
+
+export default TestimonialsComponent
