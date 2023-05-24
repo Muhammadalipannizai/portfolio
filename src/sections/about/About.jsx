2196d90925218fd6b9ff5a29cb75034673443525
Muhammad Ali <muhammadalipannizai@gmail.com>
2023-05-24 19:37:31 +0200

    fix : bug fixes

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,34 +1,41 @@
 import React from "react";
-import Profile from "../../assets/ali.jpg";
+import Profile from "../../assets/web.jpg";
 import "./about.css";
 import CV from "../../assets/cv.pdf";
+import Cards from '../../components/Cards'; 
+import data from './data';
 import FileDownloadIcon from "@mui/icons-material/FileDownload";
 
 function About() {
   return (
     <section id="about">
       <div className="container about_container">
-        <div className="about_left">
+      <div className="about_left">
           <div className="image_about">
             <img src={Profile} alt="aboutMe"></img>
           </div>
         </div>
-
         <div className="about_right">
           <h2> About me </h2>
+          <div className='about_cards'>
+            { data.map( item => (
+              <Cards key ={item.id} className = ' card_component'>
+                <span  className='icon_about'>{item.icon}</span>
+                <h5>{item.title}</h5>
+                {/* <small>{item.desc}</small> */}
+              </Cards>
+            )
+            )}
+          </div>
           <div className="about_cards"></div>
           <p>
             Hi, I'm Muhammad Ali, a junior frontend React developer passionate
             about creating engaging and user-friendly web experiences. With a
             strong foundation in HTML, CSS, and JavaScript, I strive to build
             responsive and accessible websites that meet modern web standards.
-          </p>
-          <p>
             I have hands-on experience working with React, including creating
             reusable components, managing state using hooks, and utilizing
-            third-party libraries and APIs. I am enthusiastic about leveraging
-            React's component-based architecture to develop dynamic and
-            interactive web applications.
+            third-party libraries and APIs.
           </p>
 
           <a href={CV} download className="btn black">
@@ -37,6 +44,7 @@ function About() {
             Download CV
           </a>
         </div>
+        
       </div>
     </section>
   );
--- src/sections/about/data.js
+++ src/sections/about/data.js
@@ -1,12 +1,13 @@
 
-import {FaAward} from 'react-icons/fa';
-import {TbBooks} from 'react-icons/tb';
-import {BiHappyHeartEyes} from 'react-icons/bi';
+import {FiFigma} from 'react-icons/fi';
+
+import { SiApachehive } from "react-icons/si";
+import { DiReact } from "react-icons/di";
 
 const data = [
-    { id : '1' , icon :<FaAward/> ,   title : 'Experience' , desc : ' 2 years of experience'  },
-    { id : '1' , icon : <TbBooks/> ,   title : 'Projects' , desc : ' 2 years of experience'  },
-    { id : '1' , icon :<BiHappyHeartEyes/> ,   title : 'Clients' , desc : ' 2 years of experience'  }
+    { id : '1' , icon :<DiReact/> ,   title : 'Frontend Developer' , desc : ' 2 years of experience'  },
+    { id : '1' , icon : <FiFigma/> ,   title : 'Web Designer' , desc : ' 2 years of experience'  },
+    { id : '1' , icon :<SiApachehive/> ,   title : 'Big Data Analyst' , desc : ' 2 years of experience'  }
 ]
 
 export default data;
